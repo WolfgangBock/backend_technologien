3e4fcd07b51f582d4b79e457a69542046adc3258
Wolfgang Bock <wolfgang.bock@htwchur.ch>
2020-10-08 21:00:56 +0200

    Ordner test entfernt und pdo.php hinzugefügt

---
+++ pdo.php
@@ -0,0 +1,26 @@
+<?php
+
+$db_host     = '';      // Hostserver, auf dem die DB läuft.
+                          // «localhost» bedeutet: die selbe Serveradresse, auf dem auch die Seiten gespeichert sind
+$db_name     = '';      // Name der Datenbank (stimmt im Beispiel nur zufällig mit username überein)
+$db_user     = '';      // Name des DB-Users (stimmt im Beispiel nur zufällig mit Datenbankname überein)
+$db_pass     = '';      // Passwort des DB-Users
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <title>Datenbankverbindung mit PDO</title>
+  <link rel="stylesheet" href="finale_Version/css/style.css" >
+</head>
+<body>
+  <div class="container">
+    <h1>Backend Technologien - Übung PDO</h1>
+
+
+  </div>
+</body>
+</html>
